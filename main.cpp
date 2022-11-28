349136c1a84632c517583a2541558ef554a5ede0
ZachL <lamparzj@mail.uc.edu>
2022-11-28 18:22:58 -0500

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "What is love?" << endl;
+	cout << "Baby don't hurt me, don't hurt me..." << endl;
+	cout << "   (no more)." << endl;
+
+	return 0;
+}
