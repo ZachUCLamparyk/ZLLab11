9cc75b0efebb0bc5624ff3514af06f3c7bd568fe
ZachUCLamparyk <111921134+ZachUCLamparyk@users.noreply.github.com>
2022-11-28 18:58:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
 
 	cout << "What is love?" << endl;
 	cout << "Baby don't hurt me, don't hurt me..." << endl;
-	cout << "   (no more)." << endl;
+	cout << "   (no more no more)." << endl;
 
 	return 0;
 }
